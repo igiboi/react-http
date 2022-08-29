fbd07f6961fd33ed64216601aaa67229e458a65f
Luigi Matias <luigi.matias@codehunter.com>
2022-08-29 12:36:31 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-expressions */
+import React, {useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+  const response = await fetch('https://swapi.dev/api/films');
+   const data = await response.json();
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releseDate: movieData.relese_date
+      };
+    })
+    setMovies(transformedMovies);
+    setIsLoading(false);
+ }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
